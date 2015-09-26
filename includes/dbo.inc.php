8fbc3a2ce30983557042e7b73f5c38d1086c4707
Oleg Novoselcev <oleg.novoselcev@hg.reikartz.com>
2015-09-26 19:20:35 +0300

    fix digit check changed mysql connection type

--- includes/dbo.inc.php
+++ includes/dbo.inc.php
@@ -3,18 +3,6 @@
 $login = 'nomera';
 $pwd = '1474147';
 
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=nomera', $login, $pwd);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->exec('SET NAMES "utf8"');
-} catch (PDOException $e) {
-    $error = 'Couldn\'t connect: ' . $e->getMessage();
-    include '../error.html.php';
-    exit();
-}
-/**
- * Created by PhpStorm.
- * User: Borm
- * Date: 24.09.2015
- * Time: 23:48
- */
+mysql_connect('localhost', $login, $pwd) or die(mysql_error());
+mysql_select_db('nomera');
+mysql_query('SET NAMES "UTF8"');
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+/*error_reporting(E_ALL);
+ini_set('display_errors', 1);*/
 
 //connect to db
 include 'includes/dbo.inc.php';
@@ -9,22 +11,22 @@ if (isset($_GET['add'])) {
     exit();
 }
 if (isset($_GET['edit'])) {
+    var_dump($_SERVER);die;
     include 'edit.html.php';
     exit();
 }
 
 //adding a row from form
 if (isset($_POST['numname'])) {
-    try {
-        $sql = 'INSERT INTO nomera SET numname = :numname, number = :number, adddate = CURDATE()';
-        $s = $pdo->prepare($sql);
-        $s->bindValue(':numname', $_POST['numname']);
-        $s->bindValue(':number', $_POST['number']);
-        $s->execute();
-    } catch (PDOException $e) {
-        $error = 'Adding error: ' . $e->getMessage();
+    $name = mysql_real_escape_string($_POST['numname']);
+    $number = mysql_real_escape_string($_POST['number']);
+    if (!preg_match('/(^\d+$)/', $number)) {
+        $error = 'YOU NEED TO USE ONLY 0-9 DIGITS!!1';
         include 'error.html.php';
+        die;
     }
+    $sql = "INSERT INTO nomera SET numname = '$name', number = '$number', adddate = CURDATE()";
+    mysql_query($sql);
 }
 
 //delete row by id
@@ -46,25 +48,25 @@ if (isset($_GET['delete'])) {
 
 //edit row
 if (isset($_POST['nameedit']) or isset($_POST['numberedit'])) {
-    $sql = 'UPDATE nomera SET numname = :numname, number = :number WHERE id = :id';
-    $s = $pdo->prepare($sql);
+    $id = mysql_real_escape_string($_POST['id']);
+    $name = mysql_real_escape_string($_POST['nameedit']);
+    $number = mysql_real_escape_string($_POST['numberedit']);
+    $sql = "UPDATE nomera SET numname = '$name', number = '$number' WHERE id = '$id'";
+    mysql_query($sql);
+    /*$s = $pdo->prepare($sql);
     $s->bindValue(':numname', $_POST['nameedit']);
     $s->bindValue(':number', $_POST['numberedit']);
     $s->bindValue(':id', $_POST['id']);
-    $s->execute();
+    $s->execute();*/
 }
 
 //display all data
-try {
-    $sql = 'SELECT id, numname, number FROM nomera';
-    $result = $pdo->query($sql);
-} catch (PDOException $e) {
-    $error = 'SELECT error: ' . $e->getMessage();
-    include "error.html.php";
-    exit();
-}
+$sql = 'SELECT id, numname, number FROM nomera';
+$result = mysql_query($sql) or die(mysql_error());
+
 $list = [];
-while ($row = $result->fetch()) {
+while ($row = mysql_fetch_assoc($result)) {
     $list[] = array('numname' => $row['numname'], 'number' => $row['number'], 'id' => $row['id']);
 }
+
 include 'list.html.php';
